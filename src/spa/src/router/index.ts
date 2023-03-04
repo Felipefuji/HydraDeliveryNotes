8bf660c19ce5e9137b7d03641257cd722bd33ec3
Felipe Bernal <33207748+Felipefuji@users.noreply.github.com>
2023-03-04 19:36:37 +0100

    add login page

--- src/spa/src/router/index.ts
+++ src/spa/src/router/index.ts
@@ -1,18 +1,28 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from "../views/LoginView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import(/* webpackChunkName: "about" */ "../views/RegisterView.vue"),
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: () => import(/* webpackChunkName: "about" */ "../views/DashboardView.vue"),
+    meta: {
+      authRequired: true,
+    },
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
@@ -21,5 +31,20 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+    const auth = localStorage.getItem('user');
+
+    if (to.matched.some((record) => record.meta.authRequired)) {
+        if (auth) {
+            next();
+        } else {
+            alert("You've must been logged to access this area");
+            router.push("/");
+        }
+    } else {
+        next();
+    }
+});
 
 export default router
---
+++ src/spa/src/views/DashboardView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="text-center">You're now logged as</div>
+    <div id="username_display" class="display-6">{{ email }}</div>
+    <button id="sign_out" class="mt-4 btn btn-danger" @click="signOut">
+      Logout
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { userService } from "@/services/userservices";
+
+export default {
+  data() {
+    return {
+      email: 'pruebaEmail',
+    };
+  },
+  methods: {
+    signOut() {
+      userService.logout();
+    },
+  },
+};
+</script>
