973f1b482c55221777e97ed5624e1a6018de7392
Felipe Bernal <33207748+Felipefuji@users.noreply.github.com>
2023-03-06 20:57:42 +0100

    add vuestic

--- src/spa/src/main.ts
+++ src/spa/src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createVuestic, createIconsConfig  } from "vuestic-ui";
+import "vuestic-ui/styles/essential.css";
+import "vuestic-ui/styles/typography.css";
+import "vuestic-ui/styles/smart-helpers.css";
+import "material-design-icons-iconfont/dist/material-design-icons.min.css";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(createVuestic()).mount('#app')
